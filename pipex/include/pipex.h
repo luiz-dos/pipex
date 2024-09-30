a41cb5e4c21c25f426b9adb7be7d6d5ed544896d
Luiz Maciel <52587596+luiz-dos@users.noreply.github.com>
2024-09-30 13:53:35 +0100

    Update pipex.h

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -24,5 +24,10 @@ void	child_process(int *fd, char **av, char **envp);
 void	parent_process(int *fd, char **av, char **envp);
 void	execute_command(char *av, char **envp);
 char	*get_command_path(char *cmd, char **envp);
+void	check_envp(char **envp);
+int     find_in_envp(char **envp);
+void	handle_dev_random(void);
+void	check_commands(char *av);
+int     only_space_av(char *str);
 
 #endif /* PIPEX_H */
